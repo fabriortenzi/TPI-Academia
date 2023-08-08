8b347875ff26fb7f90692fa0ec56c05d51e1edef
Valentin <elcapo_valentin@hotmail.com>
2023-08-08 19:15:43 -0300

    Form Crear curso fixed (falta que muestre los planes disponibles para la especialidad elegida)

--- TPI/Escritorio/formCrearCurso.cs
+++ TPI/Escritorio/formCrearCurso.cs
@@ -21,19 +21,50 @@ namespace Escritorio
 
         private void formCrearCurso_Load(object sender, EventArgs e)
         {
+            
+
+            TPI.Datos.Materia.inicializarMaterias();
+
             var especialidades = TPI.Negocio.Especialidad.GetAllEspecialidades();
+
+            var planes = TPI.Negocio.Plan.GetAllPlanes();
+
+            var materias = TPI.Negocio.Materia.Getallmaterias();
+
 
             foreach (var esp in especialidades)
             {
                 cbxEspecialidades.Items.Add(esp.descEspec);
             }
+
+            foreach (var plan in planes)
+            {
+                cbxPlanes.Items.Add(plan.descPlan);
+            }
+
+            foreach (var materia in materias)
+            {
+                cbxMaterias.Items.Add(materia.descMateria);
+            }
         }
 
         private void cbxEspecialidades_TextUpdate(object sender, EventArgs e)
         {
-            var planesPorEspecialidad = TPI.Negocio.Plan.GetPlanesPorEspecialidad(Especialidad);
+            cbxPlanes.Items.Clear(); // Limpiar los items existentes en cbxPlanes
+
+            var especialidadSeleccionada = cbxEspecialidades.SelectedItem.ToString();
             
-            cbxPlanes.Items.Add("");
+            if (especialidadSeleccionada != null)
+            {
+
+                var especialidad = TPI.Negocio.Especialidad.Getespecialidadpordesc(especialidadSeleccionada);
+
+                var planesPorEspecialidad = TPI.Negocio.Plan.GetPlanesPorEspecialidad(especialidad);
+                foreach (var plan in planesPorEspecialidad)
+                {
+                    cbxPlanes.Items.Add(plan.descPlan);
+                }
+            }
         }
     }
 }
